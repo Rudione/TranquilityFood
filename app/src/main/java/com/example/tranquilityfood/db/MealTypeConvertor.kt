a9cecef839feac0a84b34210da05c94d14a33b6c
Rudione <denisrudik01@gmail.com>
2022-05-22 18:26:52 +0300

    add database

---
+++ app/src/main/java/com/example/tranquilityfood/db/MealTypeConvertor.kt
@@ -0,0 +1,22 @@
+package com.example.tranquilityfood.db
+
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@TypeConverters
+class MealTypeConvertor {
+
+    @TypeConverter
+    fun fromAnyToString(item: Any?): String {
+        if(item == null)
+            return ""
+        return item as String
+    }
+
+    @TypeConverter
+    fun fromStringToAny(item: String?): Any {
+        if(item == null)
+            return ""
+        return item
+    }
+}
